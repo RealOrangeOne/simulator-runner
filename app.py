fa6c06512dce14ebcba2af8a98729d39f947094c
Jake Howard <git@theorangeone.net>
2021-07-28 20:28:55 +0100

    Run simulation task in the background
    This also requires only 1 be running at once

--- app.py
+++ app.py
@@ -6,18 +6,37 @@ import os
 from pathlib import Path
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from starlette.background import BackgroundTask
+import asyncio
+import datetime
+import shlex
 
 BASE_DIR = Path.cwd()
+SIMULATION_LOCK = asyncio.Lock()
 
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
 async def homepage(request):
-    return templates.TemplateResponse("index.html", {'request': request})
+    return templates.TemplateResponse("index.html", {'request': request, "simulation_running": SIMULATION_LOCK.locked()})
+
+
+async def run_simulation():
+    async with SIMULATION_LOCK:
+        now = datetime.datetime.now()
+        command = shlex.split(os.environ["COMMAND"])
+        print("Running simulation")
+        process = await asyncio.create_subprocess_exec(*command, env={
+            "OUTPUT_DIR": str(BASE_DIR / "output")
+        })
+
+        stdout, stderr = await process.communicate()
+        print("Running exited with code", process.returncode)
 
 
 async def submit(request):
-    return RedirectResponse(request.url_for("homepage"))
+    task = BackgroundTask(run_simulation)
+    return RedirectResponse(request.url_for("homepage"), background=task)
 
 
 app = Starlette(routes=[
