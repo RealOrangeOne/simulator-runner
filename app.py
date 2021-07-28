dab9fee87c70fea437857480844b78fea1f5a439
Jake Howard <git@theorangeone.net>
2021-07-28 21:45:05 +0100

    Add and run linters

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse, RedirectResponse
-from starlette.routing import Route, Mount
-import uvicorn
+import asyncio
 import os
+import shlex
 from pathlib import Path
+
+import uvicorn
+from starlette.applications import Starlette
+from starlette.background import BackgroundTask
+from starlette.middleware import Middleware
+from starlette.middleware.gzip import GZipMiddleware
+from starlette.responses import RedirectResponse
+from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-from starlette.background import BackgroundTask
-import asyncio
-import datetime
-import shlex
-from starlette.middleware.gzip import GZipMiddleware
-from starlette.middleware import Middleware
 
 BASE_DIR = Path.cwd()
 OUTPUT_DIR = BASE_DIR / "output"
@@ -27,20 +27,25 @@ def get_output_slugs():
     return sorted(OUTPUT_DIR.glob("*.html"))
 
 
-
 async def homepage(request):
     output_slugs = await asyncio.to_thread(get_output_slugs)
-    return templates.TemplateResponse("index.html", {'request': request, "simulation_running": SIMULATION_LOCK.locked(), "output_slugs": output_slugs})
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "simulation_running": SIMULATION_LOCK.locked(),
+            "output_slugs": output_slugs,
+        },
+    )
 
 
 async def run_simulation():
     async with SIMULATION_LOCK:
-        now = datetime.datetime.now()
         command = shlex.split(os.environ["COMMAND"])
         print("Running simulation")
-        process = await asyncio.create_subprocess_exec(*command, env={
-            "OUTPUT_DIR": str(OUTPUT_DIR)
-        })
+        process = await asyncio.create_subprocess_exec(
+            *command, env={"OUTPUT_DIR": str(OUTPUT_DIR)}
+        )
 
         stdout, stderr = await process.communicate()
         print("Running exited with code", process.returncode)
@@ -54,11 +59,15 @@ async def submit(request):
 
 app = Starlette(
     routes=[
-        Route('/', homepage, name="homepage"),
-        Route('/submit', submit),
-        Mount('/output', app=StaticFiles(directory=OUTPUT_DIR, check_dir=False), name="output")
+        Route("/", homepage, name="homepage"),
+        Route("/submit", submit),
+        Mount(
+            "/output",
+            app=StaticFiles(directory=OUTPUT_DIR, check_dir=False),
+            name="output",
+        ),
     ],
-    middleware=[Middleware(GZipMiddleware)]
+    middleware=[Middleware(GZipMiddleware)],
 )
 
 
